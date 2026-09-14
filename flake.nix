aa8cb76892751721758dbe52136365262e5a3186
bkaba <you@example.com>
2026-09-14 15:10:29 +0300

    switch to stable nix stage

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS microVM environments for AI coding agents";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     microvm = {
       url = "github:astro/microvm.nix";
       inputs.nixpkgs.follows = "nixpkgs";
